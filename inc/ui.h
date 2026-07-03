b4ee1a46f318b61f64003e8eb5438f097c5a959f
DS1TPT <ds1tpt@10.nate.com>
2026-07-04 05:21:54 +0900

    Oops, forgot to re-license ui.h

--- inc/ui.h
+++ inc/ui.h
@@ -4,20 +4,14 @@
  * Notice: This file is a part of "pm2regen-ed".
  *         Please check main.c or README.md for more information.
  * 
- * Copyright (C) 2026 Lee Geon-goo
+ * Copyright (C) 2026 Lee Geon-goo <github.com/DS1TPT>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * This work is provided "AS IS", WITHOUT WARRANTY OF ANY KIND. You can 
+ * redistribute it and/or modify it under the terms of the Do What The Fuck
+ * You Want To Greater Public License, as published by Lee Geon-goo 
+ * <github.com/DS1TPT>. You should have received a copy of the Do What The
+ * Fuck You Want To Greater Public License. If not, see
+ * <https://github.com/DS1TPT/wtfgpl> for more details.
  */
 
 #ifndef UI_H
